718329185238b298b631f739f77305c1307b4f1e
Raphaelle Tseng <raphaelletseng@gmail.com>
2025-07-20 16:00:21 -0400

    blog dates

--- src/pages/BlogHome.tsx
+++ src/pages/BlogHome.tsx
@@ -1,24 +1,46 @@
 import BlogCard from '../components/BlogCard';
 import type { Blog } from '../types';
 import blogIndex from '../blog/blogIndex';
-
 import { Box, Typography, Divider } from '@mui/material';
 
 const BlogHome = () => {
-  const sortedBlogPosts = [...blogIndex].sort(
+  const sorted = [...blogIndex].sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
   );
+
+  const postsByYear = sorted.reduce(
+    (groups, post) => {
+      const year = new Date(post.date).getFullYear();
+      (groups[year] ||= []).push(post);
+      return groups;
+    },
+    {} as Record<number, Blog[]>,
+  );
+
+  const years = Object.keys(postsByYear).sort((a, b) => +b - +a);
+
   return (
-    <Box sx={{ padding: 4, display: 'block' }}>
-      <Typography variant="h4" sx={{ textAlign: 'left', margin: 2 }}>
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4" textAlign="left" mb={2}>
         writings
       </Typography>
-      <Divider orientation="horizontal" sx={{ marginBottom: 2 }} flexItem />
-      {sortedBlogPosts.map((post: Blog) => (
-        <BlogCard key={post.slug} post={post} />
+
+      {years.map((year) => (
+        <Box key={year} sx={{ mb: 2 }}>
+          <Divider textAlign="right" sx={{ width: '15%', align: 'right', marginLeft: 'auto' }}>
+            <Typography variant="h6" sx={{ fontSize: '1rem' }}>
+              {year}
+            </Typography>
+          </Divider>
+          <Box sx={{ mt: 2 }}>
+            {postsByYear[+year].map((post) => (
+              <BlogCard key={post.slug} post={post} />
+            ))}
+          </Box>
+        </Box>
       ))}
-      ;
     </Box>
   );
 };
+
 export default BlogHome;
